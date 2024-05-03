bc8ef16602129ae7fa586a0d83bba934f25276cd
Diego Magdaleno <diegomagdaleno@protonmail.com>
2024-05-03 08:06:45 -0600

    Servicio de citas de mascotas

---
+++ src/app/model/pet.ts
@@ -0,0 +1,9 @@
+export interface Pet {
+    id: number;
+    name: string;
+    species: string;
+    breed: string;
+    age: number;
+    description: string;
+    imageUrl: string;
+}
--- src/app/serv/appointments.service.ts
+++ src/app/serv/appointments.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Appointment } from '../model/appointment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,33 @@ import { Injectable } from '@angular/core';
 export class AppointmentsService {
 
   constructor() { }
+
+  getAppointments(): Appointment[] {
+    return JSON.parse(localStorage.getItem('appointments') || '[]');
+  }
+
+  saveAppointments(appointments: Appointment[]): void {
+    localStorage.setItem('appointments', JSON.stringify(appointments));
+  }
+
+  addAppointment(appointment: Appointment): void {
+    const appointments = this.getAppointments();
+    appointments.push(appointment);
+    this.saveAppointments(appointments);
+  }
+
+  getAppointment(id: number): Appointment {
+    const appointments = this.getAppointments();
+    return appointments.find(a => a.id === id) as Appointment;
+  }
+
+  getAppointmentsAt(date: string): Appointment[] {
+    const appointments = this.getAppointments();
+    return appointments.filter(a => a.date === date);
+  }
+
+  getAppointmentsForPet(petId: number): Appointment[] {
+    const appointments = this.getAppointments();
+    return appointments.filter(a => a.petId === petId);
+  }
 }
