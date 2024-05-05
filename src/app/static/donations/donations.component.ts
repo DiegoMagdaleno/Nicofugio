35b4e1bc61c1450e10e0a2b6796de782649091ff
Diego Magdaleno <diegomagdaleno@protonmail.com>
2024-05-05 14:36:13 -0600

    Mejoras significativas al diseno

--- src/app/static/donations/donations.component.ts
+++ src/app/static/donations/donations.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../comp/navbar/navbar.component';
+import { FooterComponent } from '../../comp/footer/footer.component';
 
 @Component({
   selector: 'app-donations',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './donations.component.html',
   styleUrl: './donations.component.css'
 })
