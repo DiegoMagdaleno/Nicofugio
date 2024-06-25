cf8534dba83f3c5eb1c1f3f8783991c2d030e4a8
Diego Magdaleno <diegomagdaleno@protonmail.com>
2024-06-25 14:01:41 -0600

    Stats working

--- front/src/app/admin/admin-query/admin-query.component.ts
+++ front/src/app/admin/admin-query/admin-query.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatsService } from '../../serv/stats.service';
 
 @Component({
   selector: 'app-admin-query',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './admin-query.component.html',
   styleUrl: './admin-query.component.scss'
 })
-export class AdminQueryComponent {
+export class AdminQueryComponent implements OnInit {
+  totalUsers: number = 0;
+  mostPopularSpecie: string = '';
+  totalAppointments: number = 0;
 
+  constructor(private stats: StatsService) {}
+
+  ngOnInit() {
+    this.stats.getAppointmentsLastMonth().subscribe((data) => {
+      this.totalAppointments = data.totalAppointments;
+    });
+
+    this.stats.getUsersLastMonth().subscribe((data) => {
+      this.totalUsers = data.totalUsers;
+    });
+
+    this.stats.getMostPopularSpecie().subscribe((data) => {
+      this.mostPopularSpecie = data.mostPopularSpecies;
+    });
+  }
 }
---
+++ front/src/app/serv/stats.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { WebAPIService } from './web/web-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatsService {
+
+  baseURL = "http://localhost:3000"
+
+  constructor(private web: WebAPIService) { }
+
+  getUsersLastMonth() {
+    return this.web.get(`${this.baseURL}/stats/users`)
+  }
+
+  getMostPopularSpecie() {
+    return this.web.get(`${this.baseURL}/stats/popular`)
+  }
+
+  getAppointmentsLastMonth() {
+    return this.web.get(`${this.baseURL}/stats/app-month`)
+  }
+  
+  getStatsOfPetsAppointments() {
+    return this.web.get(`${this.baseURL}/stats`)
+  }
+}
