de9775e85879526628979c016d0b659bab15486b
Diego Magdaleno <diegomagdaleno@protonmail.com>
2024-06-24 19:57:59 -0600

    Appointments esta muerto y nosotros lo matamos

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { GalleryComponent } from './dyn/gallery/gallery.component';
 import { PetDetailsComponent } from './comp/pet-details/pet-details.component';
-import { AppointmentsComponent } from './dyn/appointments/appointments.component';
 import { HomeComponent } from './static/home/home.component';
 import { AboutComponent } from './static/about/about.component';
 import { DonationsComponent } from './static/donations/donations.component';
@@ -23,10 +22,6 @@ export const routes: Routes = [
   {
     path: 'pet-details/:id',
     component: PetDetailsComponent,
-  },
-  {
-    path: 'appointments',
-    component: AppointmentsComponent,
   },
   {
     path: 'about',
